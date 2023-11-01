d84c1cde68fd251fb76b49557060c97c49927b9f
ch4rl3sF <charlesfellows16@gmail.com>
2023-11-01 18:27:51 +0000

    Select function started. Not working

---
+++ lib/shared/workout_list.dart
@@ -0,0 +1,18 @@
+import 'package:burnboss/services/database.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
+
+class WorkoutList extends StatefulWidget {
+  const WorkoutList({Key? key}) : super(key: key);
+
+  @override
+  State<WorkoutList> createState() => _WorkoutListState();
+}
+
+class _WorkoutListState extends State<WorkoutList> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
